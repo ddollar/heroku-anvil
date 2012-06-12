b9f56691335cd55d586e11868d138e6161886666
David Dollar <ddollar@gmail.com>
2012-06-12 01:26:44 -0400

    allow process table to be overwritten

--- lib/anvil/heroku/command/release.rb
+++ lib/anvil/heroku/command/release.rb
@@ -6,12 +6,25 @@ class Heroku::Command::Release < Heroku::Command::Base
   #
   # release a slug
   #
+  # -p, --procfile PROCFILE  # use an alternate Procfile to define process types
+  #
   def index
     error("Usage: heroku release SLUG_URL") unless build_url = shift_argument
     validate_arguments!
 
     action("Releasing to #{app}") do
-      release = heroku.release(app, "Anvil deploy", :build_url => build_url)
+      release_options = { :build_url => build_url }
+
+      if options[:procfile] then
+        release_options[:processes] = File.read(options[:procfile]).split("\n").inject({}) do |ax, line|
+          if line =~ /^([A-Za-z0-9_]+):\s*(.+)$/
+            ax[$1] = $2
+          end
+          ax
+        end
+      end
+
+      release = heroku.release(app, "Anvil deploy", release_options)
       status release["release"]
     end
   end
