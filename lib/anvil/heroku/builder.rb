3af73a6174e1e08a6aea04c9cc9ef335aa0e575b
David Dollar <ddollar@gmail.com>
2012-07-15 11:04:33 -0400

    add builder

---
+++ lib/anvil/heroku/builder.rb
@@ -0,0 +1,46 @@
+require "heroku"
+
+class Heroku::Builder
+
+  def build(source, options={})
+    uri  = URI.parse("#{anvil_host}/build")
+    http = Net::HTTP.new(uri.host, uri.port)
+
+    if uri.scheme == "https"
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+
+    req = Net::HTTP::Post.new uri.request_uri
+
+    req.set_form_data({
+      "buildpack" => options[:buildpack],
+      "cache"     => options[:cache],
+      "env"       => options[:env] || {},
+      "source"    => source
+    })
+
+    slug_url = nil
+
+    http.request(req) do |res|
+      slug_url = res["x-slug-url"]
+
+      res.read_body do |chunk|
+        yield chunk
+      end
+    end
+
+    slug_url
+  end
+
+private
+
+  def anvil
+    @anvil ||= RestClient::Resource.new(anvil_host)
+  end
+
+  def anvil_host
+    ENV["ANVIL_HOST"] || "http://anvil-production.herokuapp.com"
+  end
+
+end
