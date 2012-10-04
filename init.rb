f77693a2357a58a7c25f8610a891355357c733ba
David Dollar <ddollar@gmail.com>
2012-10-04 12:54:11 -0400

    cloud isnt a thing

--- init.rb
+++ init.rb
@@ -4,6 +4,5 @@ end
 
 require "anvil/heroku/client"
 require "anvil/heroku/command/build"
-require "anvil/heroku/command/cloud"
 require "anvil/heroku/command/release"
 require "anvil/heroku/command/start"
